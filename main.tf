796a3c8a2ad561ff4d4cd49154a81d46526b10f2
Andrew Clancey <aclancey@rez1.com>
2018-05-24 10:51:58 -0400

    Template port

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "server_port" {
+  description = "The port the server wil use for HTTP requests"
+  default     = 8080
+}
+
 resource "aws_instance" "example" {
   ami                    = "ami-40d28157"
   instance_type          = "t2.micro"
@@ -9,7 +14,7 @@ resource "aws_instance" "example" {
 
   user_data = <<-EOF
               echo "Hello, World!" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
   tags {
@@ -21,9 +26,13 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
